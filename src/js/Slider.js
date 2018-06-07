bd25f141defa7f881f1faef03a3584962ac13d5a
sashatherocket <35545113+sashatherocket@users.noreply.github.com>
2018-06-08 00:23:01 +0300

    Slider.

---
+++ src/js/Slider.js
@@ -0,0 +1,42 @@
+import $ from "jquery";
+class Slider {
+  constructor() {
+    this.bullet1 = $("#bullet-1");
+    this.bullet2 = $("#bullet-2");
+    this.bullet3 = $("#bullet-3");
+    this.slide1 = $("#slide-1");
+    this.slide2 = $("#slide-2");
+    this.slide3 = $("#slide-3");
+    this.events();
+  }
+  events() {
+    this.bullet1.click(this.showSlide1.bind(this));
+    this.bullet2.click(this.showSlide2.bind(this));
+    this.bullet3.click(this.showSlide3.bind(this));
+  }
+  showSlide1() {
+    this.bullet1.addClass("slider__controls__item__active");
+    this.bullet2.removeClass("slider__controls__item__active");
+    this.bullet3.removeClass("slider__controls__item__active");
+    this.slide1.addClass("slider__slides__item__active");
+    this.slide2.removeClass("slider__slides__item__active");
+    this.slide3.removeClass("slider__slides__item__active");
+  }
+  showSlide2() {
+    this.bullet1.removeClass("slider__controls__item__active");
+    this.bullet2.addClass("slider__controls__item__active");
+    this.bullet3.removeClass("slider__controls__item__active");
+    this.slide1.removeClass("slider__slides__item__active");
+    this.slide2.addClass("slider__slides__item__active");
+    this.slide3.removeClass("slider__slides__item__active");
+  }
+  showSlide3() {
+    this.bullet1.removeClass("slider__controls__item__active");
+    this.bullet2.removeClass("slider__controls__item__active");
+    this.bullet3.addClass("slider__controls__item__active");
+    this.slide1.removeClass("slider__slides__item__active");
+    this.slide2.removeClass("slider__slides__item__active");
+    this.slide3.addClass("slider__slides__item__active");
+  }
+}
+export default Slider;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import "../css/styles.css";
 import $ from "jquery";
 import MobileMenu from "./MobileMenu.js";
+import Slider from "./Slider.js";
 $(document).ready(function() {
   var mobileMenu = new MobileMenu;
+  var slider = new Slider;
 });
